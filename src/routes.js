ca5765c1db0db6e7dfe29256cc95b3858a1a0a1e
alanfoandrade <alanfrank@gec.inatel.br>
2019-11-15 12:47:52 -0300

    Criacao da base da aplicacao

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import User from './pages/User';
+import Main from './pages/Main';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    },
+  ),
+);
+
+export default Routes;
